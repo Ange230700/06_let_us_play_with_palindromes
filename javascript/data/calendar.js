2b7de5e2e968b3bce17ece345ac37a723591874b
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-20 11:57:25 +0100

    feat: 🎸  implemented isValidDate function

---
+++ javascript/data/calendar.js
@@ -0,0 +1,5 @@
+// javascript\data\calendar.js
+
+const calendar = [31, 28, 31, 30, 31, 31, 30, 31, 30, 31, 30, 31];
+
+export default calendar;
--- main.js
+++ main.js
@@ -5,3 +5,4 @@ import isValidDate from "./javascript/functions/isValidDate.js";
 console.log(isValidDate("03/04/2001"));
 console.log(isValidDate("03/13/2001"));
 console.log(isValidDate("03/04/128"));
+console.log(isValidDate("32/01/2001"));
