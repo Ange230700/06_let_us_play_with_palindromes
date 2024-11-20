9af08cfffa872d1b139357da708f0740c67cb709
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-20 11:57:25 +0100

    feat: 🎸  implemented isValidDate function

--- javascript/functions/isValidDate.js
+++ javascript/functions/isValidDate.js
@@ -1,16 +1,28 @@
 // javascript\functions\isValidDate.js
 
+import calendar from "../data/calendar.js";
 import separateDate from "./separateDate.js";
 
 function isValidDate(date) {
   const dateSeparated = separateDate(date);
-  console.log(dateSeparated);
 
-  if (dateSeparated.monthNumber < 1 || 12 < dateSeparated.monthNumber) {
+  if (
+    parseInt(dateSeparated.monthNumber) < 1 ||
+    12 < parseInt(dateSeparated.monthNumber)
+  ) {
     return false;
   }
 
-  if (dateSeparated.yearNumber < 999 || 9999 < dateSeparated.yearNumber) {
+  if (
+    parseInt(dateSeparated.yearNumber) < 999 ||
+    9999 < parseInt(dateSeparated.yearNumber)
+  ) {
+    return false;
+  }
+
+  if (
+    calendar[dateSeparated.monthNumber - 1] < parseInt(dateSeparated.dayNumber)
+  ) {
     return false;
   }
 
