77f170633645fcfa522038edbe45259e7ec7a7a6
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-20 15:33:25 +0100

    feat: 🎸  implemented isPalindrome function

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 // main.js
 
-import isValidDate from "./javascript/functions/isValidDate.js";
+import isPalindrome from "./javascript/functions/isPalindrome";
 
-console.log(isValidDate("03/04/2001"));
-console.log(isValidDate("03/13/2001"));
-console.log(isValidDate("03/04/128"));
-console.log(isValidDate("32/01/2001"));
+console.log(isPalindrome("22/02/2022"));
