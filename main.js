67fe92eb01343f4f0fb3ebbbdd7a8242bab27f19
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-21 12:52:18 +0100

    test(the whole logic): 💍  tested all stuff

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // main.js
+
+import isPalindrome from "./javascript/functions/isPalindrome";
+import isValidDate from "./javascript/functions/isValidDate";
+import rewriteDateInProperFormat from "./javascript/functions/rewriteDateInProperFormat";
+import rewriteDateWithoutSlash from "./javascript/functions/rewriteDateWithoutSlash";
+import separateDate from "./javascript/functions/separateDate";
+import skipToNextDay from "./javascript/functions/skipToNextDay";
+import getNextPalindromes from "./javascript/functions/getNextPalindromes";
+
+console.log(separateDate("23/07/2000"));
+console.log(isValidDate("23/07/2000"));
+console.log(rewriteDateInProperFormat(separateDate("23/07/2000")));
+console.log(rewriteDateWithoutSlash(separateDate("23/07/2000")));
+console.log(isPalindrome("23/07/2000"));
+console.log(skipToNextDay("23/07/2000"));
+console.log(getNextPalindromes(10));
